4d83985510893fad447eec3a8d3d0a4d393d1eca
Mark <AgranatMarkit@gmail.com>
2016-05-06 16:41:58 +0300

    Fix repeat enter bug

--- cgi-bin/form.py
+++ cgi-bin/form.py
@@ -1,30 +1,33 @@
-﻿#!/usr/bin/env python3
+#!/usr/bin/env python3
 import cgi
 import html
 import testovoe
+import sys
+import codecs
+
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+
+
 
 form = cgi.FieldStorage()
 text1 = form.getfirst("TEXT_1")
 text1 = html.escape(text1)
-text = testovoe.main(str(text1))
+text = testovoe.main(text1)
 
-print("Content-type: text/html\n")
-print("""
-        <!DOCTYPE HTML>
+a = """
+<!DOCTYPE HTML>
         <html>
         <head>
-            
+            <meta charset = "utf-8">
             <link type="text/css" rel="stylesheet" href="../style.css">
             <title>Тестовое задание</title>
         </head>
         <body>
         <div class="center">
         <h1>Склонятор</h1>
-        <form action="/cgi-bin/form.py">
-    	<p>Введите слово:</p>
-    	""")
-print('<input type="text" name="TEXT_1" value="{}">'.format(text1))
-print("""
+        <form action="/cgi-bin/form.py" method="post">
+        <p>Введите слово:</p>
+        <input type="text" name="TEXT_1" value="%s">
         <br>
         <br>
         
@@ -34,4 +37,7 @@ print("""
         </form>
         </div>
         </body>
-        </html>"""%text)
+        </html>
+"""%(text1,text)
+print("Content-type: text/html\n")
+print(a)
--- cgi-bin/testovoe.py
+++ cgi-bin/testovoe.py
@@ -3,20 +3,13 @@ def main(value):
     morph = pymorphy2.MorphAnalyzer()
     parse = morph.parse(value)
 
-##    for i in parse.lexeme:
-##        print(i[0])
-
     forPrint = []
 
     for i in range(0,len(parse)):
         x = parse[i].lexeme
-        #print(x)
         for j in range(0,len(x)):
             if(forPrint.count(x[j][0])==0):
                forPrint.append(x[j][0])
-            #print(x[j][0])
-    
-##    for i in forPrint:
-##        print(i)
+               
     result = '\n'.join(forPrint)
     return result
